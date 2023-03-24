191130764c31c2a7c3b98cb145b884142e9d8c1a
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-03-24 14:35:07 +0800

    新增grid.py

---
+++ grid1.py
@@ -0,0 +1,42 @@
+'''
+專案在學習grid的編排
+'''
+
+import tkinter as tk
+from tkinter import ttk
+
+class Window(tk.Tk):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        ttkStyle = ttk.Style()
+        ttkStyle.theme_use('default')
+        ttkStyle.configure('red.TFrame',background='#ff0000')
+        ttkStyle.configure('white.TFrame',background='#ffffff')
+        ttkStyle.configure('yellow.TFrame',background='yellow')
+        
+        mainFrame = ttk.Frame(self)        
+        mainFrame.pack(expand=True,fill=tk.BOTH)
+        
+
+        topFrame = ttk.Frame(mainFrame,height=100)
+        topFrame.pack(fill=tk.X)
+
+        ttk.Label(topFrame,text="BMI試算",font=('Helvetica', '20')).pack(pady=20)
+
+        bottomFrame = ttk.Frame(mainFrame,style='yellow.TFrame')
+        bottomFrame.pack(expand=True,fill=tk.BOTH)
+
+
+
+
+def main():
+    '''
+    這是程式的執行點
+    '''
+    window = Window()
+    window.title("BMI計算")
+    window.geometry("400x500")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
