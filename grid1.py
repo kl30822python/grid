2147a57dccb38f511fc42984df4a3d603e117662
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-03-24 16:47:00 +0800

    add grid1.py 輸入bmi

--- grid1.py
+++ grid1.py
@@ -13,11 +13,11 @@ class Window(tk.Tk):
         ttkStyle.configure('red.TFrame',background='#ff0000')
         ttkStyle.configure('white.TFrame',background='#ffffff')
         ttkStyle.configure('yellow.TFrame',background='yellow')
-        ttkStyle.configure('gridLabel.TLabel',font=('Helvetica',16))
+        ttkStyle.configure('gridLabel.TLabel',font=('Helvetica',16),foreground='#676767')
         ttkStyle.configure('gridEntry.TLabel',font=('Helvetica',16))
         
         mainFrame = ttk.Frame(self)        
-        mainFrame.pack(expand=True,fill=tk.BOTH)
+        mainFrame.pack(expand=True,fill=tk.BOTH, padx=30, pady=30)
         
 
         topFrame = ttk.Frame(mainFrame,height=100)
@@ -27,29 +27,39 @@ class Window(tk.Tk):
 
         bottomFrame = ttk.Frame(mainFrame,style='yellow.TFrame')
         bottomFrame.pack(expand=True,fill=tk.BOTH)
-        bottomFrame.columnconfigure(0,weight=3)
-        bottomFrame.columnconfigure(1,weight=5)
-        bottomFrame.rowconfigure(0,weight=3)
+        bottomFrame.columnconfigure(0,weight=3,pad=20) #added pad=20
+        bottomFrame.columnconfigure(1,weight=5,pad=20) #added pad=20
+        bottomFrame.rowconfigure(0,weight=1,pad=20) #added pad=20
+        bottomFrame.rowconfigure(3,weight=1,pad=20) #added pad=20
+        bottomFrame.rowconfigure(4,weight=1,pad=20) #added pad=20
+        bottomFrame.rowconfigure(5,weight=1,pad=20) #added pad=20
+        bottomFrame.rowconfigure(6,weight=1,pad=20) #added pad=20
 
-        ttk.Label(bottomFrame, text='姓名:', style='gridLabel.TLabel').grid(column=0,row=0,sticky=tk.E)
-        nameEntry = ttk.Entry(bottomFrame, style='gridEntry.TEntry')
-        nameEntry.grid(column=1, row=0, sticky=tk.W)
+        ttk.Label(bottomFrame, text='姓名：', style='gridLabel.TLabel').grid(column=0,row=0,sticky=tk.E)
+        nameEntry = ttk.Entry(bottomFrame, style='gridEntry.TEntry') 
+        nameEntry.grid(column=1, row=0, sticky=tk.W, padx=10) #added padx=10
 
-        ttk.Label(bottomFrame, text="出生年月日:", style='gridLabel.TLabel').grid(column=0,row=1, sticky=tk.E)
+        ttk.Label(bottomFrame, text="出生年月日：", style='gridLabel.TLabel').grid(column=0,row=1, sticky=tk.E)
         ttk.Label(bottomFrame, text="(2000/03/01)", style='gridLabel.TLabel').grid(column=0,row=2, sticky=tk.E)
 
         birthEntry = ttk.Entry(bottomFrame, style='gridEntry.TEntry')
-        birthEntry.grid(column=1, row=1, sticky=tk.W, rowspan=2)
+        birthEntry.grid(column=1, row=1, sticky=tk.W, rowspan=2, padx=10) #added padx=10
 
-        ttk.Label(bottomFrame, text='身高(cm):', style='gridLabel.TLabel').grid(column=0,row=3,sticky=tk.E)
+        ttk.Label(bottomFrame, text='身高(cm)：', style='gridLabel.TLabel').grid(column=0,row=3,sticky=tk.E)
         heightEntry = ttk.Entry(bottomFrame, style='gridEntry.TEntry')
-        heightEntry.grid(column=1, row=3, sticky=tk.W)
+        heightEntry.grid(column=1, row=3, sticky=tk.W, padx=10) #added padx=10
 
-        ttk.Label(bottomFrame, text='體重(kg):', style='gridLabel.TLabel').grid(column=0,row=4,sticky=tk.E)
+        ttk.Label(bottomFrame, text='體重(kg)：', style='gridLabel.TLabel').grid(column=0,row=4,sticky=tk.E)
         heightEntry = ttk.Entry(bottomFrame, style='gridEntry.TEntry')
-        heightEntry.grid(column=1, row=4, sticky=tk.W)
+        heightEntry.grid(column=1, row=4, sticky=tk.W, padx=10) #added padx=10
 
+        messageText = tk.Text(bottomFrame, height=5, width=35, state=tk.DISABLED)
+        messageText.grid(column=0, row=5, sticky=tk.N+tk.S, columnspan=2)
 
+        commitBtn = ttk.Button(bottomFrame, text="計算")
+        commitBtn.grid(column=1, row=6, sticky=tk.W)
+        
+    
 
 def main():
     '''
